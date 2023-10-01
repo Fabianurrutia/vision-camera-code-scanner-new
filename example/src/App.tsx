8bd9fadb11fd793112526d072d2424133ea3aaa2
Fabian Urrutia <fabian.urrutia@live.cl>
2023-10-01 00:16:24 -0300

    new version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,46 @@
-import * as React from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
-import { StyleSheet, Text } from 'react-native';
-import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { Platform, StyleSheet, Text } from 'react-native';
+import { Camera, useCameraDevice, useCameraFormat } from 'react-native-vision-camera';
 import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from './Constants'
 
 export default function App() {
-  const [hasPermission, setHasPermission] = React.useState(false);
+  const [hasPermission, setHasPermission] = useState(false);
+
   const [frameProcessor, barcodes] = useScanBarcodes([
     BarcodeFormat.ALL_FORMATS,
     BarcodeFormat.QR_CODE,
   ]);
-  const devices = useCameraDevices();
-  const device = devices.back;
 
-  React.useEffect(() => {
+  const [cameraPosition, ] = useState<'front' | 'back'>('back');
+
+  const device = useCameraDevice(cameraPosition, {
+    physicalDevices: ['ultra-wide-angle-camera', 'wide-angle-camera', 'telephoto-camera'],
+  });
+
+  const [targetFps, ] = useState(30);
+
+  const screenAspectRatio = SCREEN_HEIGHT / SCREEN_WIDTH;
+  const format = useCameraFormat(device, [
+    { fps: targetFps },
+    { videoAspectRatio: screenAspectRatio },
+    { videoResolution: 'max' },
+    { photoAspectRatio: screenAspectRatio },
+    { photoResolution: 'max' },
+  ]);
+
+  const fps = Math.min(format?.maxFps ?? 1, targetFps);
+  
+  useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
       setHasPermission(status === 'granted');
     })();
   }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     console.log(barcodes);
   }, [barcodes]);
 
@@ -31,9 +51,12 @@ export default function App() {
         <Camera
           style={StyleSheet.absoluteFill}
           device={device}
+          format={format}
+          fps={fps}
           isActive={true}
-          pixelFormat={'yuv'}
+          orientation="portrait"
           frameProcessor={frameProcessor}
+          pixelFormat={Platform.OS === "ios" ? "native" : "yuv"}
         />
         {barcodes.map((barcode, idx) => (
           <Text key={idx} style={styles.barcodeTextURL}>
